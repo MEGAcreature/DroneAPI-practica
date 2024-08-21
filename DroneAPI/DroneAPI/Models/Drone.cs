32ed8ac81a7ba7ad1fe95a73da0d88c18d246a7b
MEGAcreature <szabotsaba@gmail.com>
2024-08-21 11:31:57 +0300

    added temporary DroneService and finished DroneController

---
+++ DroneAPI/DroneAPI/Models/Drone.cs
@@ -0,0 +1,14 @@
+﻿namespace DroneAPI.Models
+{
+    public class Drone
+    {
+        public int Id { get; set; }
+     
+        public int SerialNumber { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? Owner { get; set; }
+
+    }
+}
---
+++ DroneAPI/DroneAPI/Services/DroneService.cs
@@ -0,0 +1,48 @@
+﻿using DroneAPI.Models;
+
+namespace DroneAPI.Services
+{
+    public class DroneService
+    {
+        static List<Drone> Drones {  get; }
+        static int nextId = 4;
+        static DroneService()
+        {
+            Drones = new List<Drone>
+            {
+                new Drone() {Id = 0, SerialNumber = 123, Model = "Fly", Owner = "Csaba"},
+                new Drone() {Id = 1, SerialNumber = 145, Model = "Fly", Owner = "Csaba"},
+                new Drone() {Id = 2, SerialNumber = 155, Model = "Fly", Owner = "Csaba"},
+                new Drone() {Id = 3, SerialNumber = 167, Model = "Fly", Owner = "Csaba"},
+            };
+        }
+
+        public static List<Drone> GetAll() => Drones;
+
+        public static Drone? Get(int id) => Drones.FirstOrDefault(d => d.Id == id);
+    
+        public static void Add(Drone drone)
+        {
+            drone.Id = nextId++;
+            Drones.Add(drone);
+        }
+
+        public static void Delete(int id)
+        {
+            var drone = Get(id);
+            if (drone is null)
+                return;
+            Drones.Remove(drone);
+        }
+
+        public static void Update(Drone drone)
+        {
+            var index = Drones.FindIndex(d => d.Id == drone.Id);
+            if (index < 0)
+            {
+                return;
+            }
+            Drones[index] = drone;
+        }
+    }
+}
