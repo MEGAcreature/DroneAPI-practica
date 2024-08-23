30c4f3ba0b9084fda3f4d12ff10ebbd4d3509011
MEGAcreature <szabotsaba@gmail.com>
2024-08-23 10:07:42 +0300

    fixed ? warnings

--- DroneAPI/DroneAPI/Models/Snapshot.cs
+++ DroneAPI/DroneAPI/Models/Snapshot.cs
@@ -2,12 +2,12 @@
 {
     public class Snapshot
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public float Longitude { get; set; }
         public float Altitude{ get; set; }
         public float Latitude { get; set; }
-        public Drone Drone { get; set; }    
+        public Drone? Drone { get; set; }    
         public DateTime DateTime { get; set; }
-        public string Description { get; set; } 
+        public string? Description { get; set; } 
     }
 }
